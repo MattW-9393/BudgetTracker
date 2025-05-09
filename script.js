8a134592739a693ad32217f736798cb010525b53
mattw-9393 <matt.warne22@gmail.com>
2025-05-09 09:16:15 +0100

    store income value to local memory

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let myBalance = document.getElementById("income");
 let balanceValueNum = 0
 
 function getBalance () {
+    // get balance from income input and display in Transaction Table 
     balanceValueNum = myBalance.value
     document.getElementById("balanceValue").innerHTML = "Balance: £" + balanceValueNum
-    console.log("Balance set to £" + balanceValueNum)
     //save balance to memory
+    localStorage.removeItem("balanceValueNum")
+    localStorage.setItem("balanceValueNum", balanceValueNum)
+
+    //Check balance saved to memory
+    const getBalance = localStorage.getItem("balanceValueNum")
+    console.log("balance is £" + getBalance)
 }
+
